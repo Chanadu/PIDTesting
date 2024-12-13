d4475a48723ae9830b3f74b087f6ee7a7fa6a2f5
Chanadu <73862221+Chanadu@users.noreply.github.com>
2024-12-13 17:56:35 +0000

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 void disabled() {}
 void competition_initialize() {}
-void autonomous() {}
+
 
 const float drivetrainWheelSize = 2.75;
 const float distanceToTravelInches = 48;
@@ -13,14 +13,6 @@ pros::Controller controller(pros::E_CONTROLLER_MASTER);
 pros::MotorGroup leftMotorGroup({1, -12, 3}, pros::MotorGears::blue);
 pros::MotorGroup rightMotorGroup({-4, 5, -8}, pros::MotorGears::blue);
 pros::Imu imu(11);
-
-struct PID {
-	double kP, kI, kD;
-	double target;
-	double previousError = 0;
-	double integral = 0;
-	double antiWindup = 3;
-};
 
 double getAveragePosition() {
 	std::vector<double> leftPositions = leftMotorGroup.get_position_all();
@@ -33,81 +25,145 @@ double getAveragePosition() {
 	return averagePosition;
 }
 
-double updateDrivePID(PID* pid) {
-	// Calculate error
-	double error = getAveragePosition() - pid->target;
+class PIDImpl;
+class PID {
+    public:
+        // Kp -  proportional gain
+        // Ki -  Integral gain
+        // Kd -  derivative gain
+        // dt -  loop interval time
+        // max - maximum value of manipulated variable
+        // min - minimum value of manipulated variable
+        PID( double dt, double max, double min, double Kp, double Kd, double Ki );
 
-	// Calculate proportional term
-	double proportional = pid->kP * error;
+        // Returns the manipulated variable given a setpoint and current process value
+        double calculate( double setpoint, double pv );
+        ~PID();
 
-	// Calculate integral term
-	pid->integral += pid->kI * error;
+    private:
+        PIDImpl *pimpl;
+};
 
-	// Calculate derivative term
-	double derivative = pid->kD * (error - pid->previousError);
+class PIDImpl
+{
+    public:
+        PIDImpl( double dt, double max, double min, double Kp, double Kd, double Ki );
+        ~PIDImpl();
+        double calculate( double setpoint, double pv );
 
-	// Calculate PID output
-	double output = proportional + pid->integral + derivative;
+    private:
+        double _dt;
+        double _max;
+        double _min;
+        double _Kp;
+        double _Kd;
+        double _Ki;
+        double _pre_error;
+        double _integral;
+};
 
-	// Update previous error
-	pid->previousError = error;
 
-	return output;
+PID::PID(double dt, double max, double min, double Kp, double Kd, double Ki )
+{
+    pimpl = new PIDImpl(dt,max,min,Kp,Kd,Ki);
+}
+double PID::calculate( double setpoint, double pv )
+{
+    return pimpl->calculate(setpoint,pv);
+}
+PID::~PID() 
+{
+    delete pimpl;
 }
 
-double updateTurnPID(PID* pid) {
-	double angle = imu.get_heading();
-	double error = pid->target - angle;
 
-	// Calculate proportional term
-	double proportional = pid->kP * error;
+/**
+ * Implementation
+ */
+PIDImpl::PIDImpl( double dt, double max, double min, double Kp, double Kd, double Ki ) :
+    _dt(dt),
+    _max(max),
+    _min(min),
+    _Kp(Kp),
+    _Kd(Kd),
+    _Ki(Ki),
+    _pre_error(0),
+    _integral(0)
+{
+}
 
-	// Calculate integral term
-	pid->integral += pid->kI * error;
+double PIDImpl::calculate( double setpoint, double pv )
+{
+    
+    // Calculate error
+    double error = setpoint - pv;
 
-	// Calculate derivative term
-	double derivative = pid->kD * (error - pid->previousError);
+    // Proportional term
+    double Pout = _Kp * error;
 
-	// Calculate PID output
-	double output = proportional + pid->integral + derivative;
+    // Integral term
+    _integral += error * _dt;
+    double Iout = _Ki * _integral;
 
-	// Update previous error
-	pid->previousError = error;
+    // Derivative term
+    double derivative = (error - _pre_error) / _dt;
+    double Dout = _Kd * derivative;
 
-	return std::clamp(output, -1.0, 1.0);
+    // Calculate total output
+    double output = Pout + Iout + Dout;
+
+    // Restrict to max/min
+    if( output > _max )
+        output = _max;
+    else if( output < _min )
+        output = _min;
+
+    // Save error to previous error
+    _pre_error = error;
+
+    return output;
+}
+
+PIDImpl::~PIDImpl()
+{
+}
+
+void pre_auton(void) {
+// Initializing Robot Configuration. DO NOT REMOVE!
+vexcodeInit();
+
+	leftMotorGroup.set_encoder_units(pros::E_MOTOR_ENCODER_DEGREES);
+	rightMotorGroup.set_encoder_units(pros::E_MOTOR_ENCODER_DEGREES);
+	leftMotorGroup.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
+	rightMotorGroup.set_brake_mode(pros::E_MOTOR_BRAKE_BRAKE);
+
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+}
+
+void drivePID() {
+    PID lPID = PID(0.1, 100, -100, 0.1, 0.01, 0.5);
+    PID tPID = PID(0.1, 100, -100, 0.1, 0.01, 0.5);
+	int lTarget = 2;
+	int tTarget = 0;
+
+	double lateralMotorPower = lPID.calculate(lTarget, getAveragePosition());
+	double turnMotorPower = tPID.calculate(tTarget, imu.get_heading());
+	leftMotorGroup.move(lateralMotorPower + turnMotorPower);
+    rightMotorGroup.move(lateralMotorPower - turnMotorPower);
+
+
+	pros::delay(10);
+}
+
+void autonomous() {
+	pros::Task a(drivePID);
 }
 
 void opcontrol() {
-	leftMotorGroup.set_encoder_units(pros::E_MOTOR_ENCODER_DEGREES);
-	rightMotorGroup.set_encoder_units(pros::E_MOTOR_ENCODER_DEGREES);
-
-	imu.reset();
-	leftMotorGroup.tare_position();
-	rightMotorGroup.tare_position();
-
-	// Kp, Ki, Kd, target
-	PID drivePID = {0.1, 0.1, 0.1, 360 * (distanceToTravelInches / drivetrainWheelSize)};
-	PID turnPID = {0.1, 0.1, 0.1, 0};
 
 	while (true) {
-		pros::lcd::print(0, "Started");
-
-		double drive = updateDrivePID(&drivePID) * 12000 * movementSpeed;
-		double turn = updateTurnPID(&turnPID) * 12000 * movementSpeed;
-
-		if (getAveragePosition() <= drivePID.target + errorRange &&
-			getAveragePosition() >= drivePID.target - errorRange) {
-			drive = 0;
-			turn = 0;
-		}
-
-		pros::lcd::print(1, "Position: %f", getAveragePosition());
-		pros::lcd::print(2, "D: %f, T: %f", drive, turn);
-
-		// Drive
-		leftMotorGroup.move_voltage(drive + turn);
-		rightMotorGroup.move_voltage(drive - turn);
-
+		
 		pros::delay(20);
 	}
 }
